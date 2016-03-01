c75b9557fcb87b57a92c2160dc0194b4bc4a6a71
akto1993 <akto1993@gmail.com>
2016-03-01 12:02:20 +0100

    po pierwszych zajęciach

--- lab01/zad1/Tests/yolo/CalculatorTest.java
+++ lab01/zad1/Tests/yolo/CalculatorTest.java
@@ -9,6 +9,58 @@ import org.junit.*;
  */
 public class CalculatorTest {
     Calculator calc = new Calculator();
+    @Test
+    public void testAdd() throws Exception {
+        assertEquals(5, calc.add(2,3));
+        assertEquals(3, calc.add(-2,5));
+        assertEquals(4, calc.add(6,-2));
+        assertEquals(-8, calc.add(-6,-2));
+        assertEquals(2, calc.add(0,2));
+        assertEquals(2, calc.add(2,0));
+        assertEquals(0, calc.add(0,0));
+        assertNotNull(calc.add(0,0));
+    }
+
+    @Test
+    public void testSub() throws Exception {
+        assertEquals(-1, calc.sub(2,3));
+        assertEquals(-7, calc.sub(-2,5));
+        assertEquals(8, calc.sub(6,-2));
+        assertEquals(-4, calc.sub(-6,-2));
+        assertEquals(-2, calc.sub(0,2));
+        assertEquals(2, calc.sub(2,0));
+        assertEquals(0, calc.sub(0,0));
+        assertNotNull(calc.sub(0,0));
+    }
+
+    @Test
+    public void testMulti() throws Exception {
+        assertEquals(6, calc.multi(2,3));
+        assertEquals(-10, calc.multi(-2,5));
+        assertEquals(-12, calc.multi(6,-2));
+        assertEquals(12, calc.multi(-6,-2));
+        assertEquals(0, calc.multi(0,2));
+        assertEquals(0, calc.multi(2,0));
+        assertEquals(0, calc.multi(0,0));
+        assertNotNull(calc.multi(0,0));
+    }
+
+    @Test
+    public void testDiv() throws Exception {
+        assertEquals(2, calc.div(6,3));
+        assertEquals(-2, calc.div(-5,2));
+        assertEquals(-3, calc.div(6,-2));
+        assertEquals(3, calc.div(-6,-2));
+        assertEquals(0, calc.div(0,2));
+        assertNotNull(calc.div(1,1));
+    }
+
+    @Test
+    public void testGreater() throws Exception {
+        assertFalse(calc.greater(2,5));
+        assertTrue(calc.greater(3,2));
+    }
+    /*Calculator calc = new Calculator();
     @Before
     public void setUp(){
         System.out.println("Before ");
@@ -37,5 +89,5 @@ public class CalculatorTest {
     public void testSubstr() throws Exception {
         assertEquals(6, calc.add(3,3));
         System.out.println("Substr " + calc);
-    }
+    }*/
 }
--- lab01/zad1/src/yolo/Calculator.java
+++ lab01/zad1/src/yolo/Calculator.java
@@ -4,7 +4,19 @@ package yolo;
  * Created by Greg on 01.03.2016.
  */
 public class Calculator {
-    public int add(int l1,int l2){
-        return l1+l2;
+    public int add(int a,int b){
+        return a+b;
+    }
+    int sub(int a, int b) { return a-b; }
+    int multi(int a, int b) { return a*b; }
+    int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argument 'b' is 0");
+        }
+        else return a/b;
+    }
+    boolean greater(int a, int b) {
+        if (a>b) return true;
+        else return false;
     }
 }

44a903820db75d57cb291dc9862e4cd9a51db817
akto1993 <akto1993@gmail.com>
2016-03-15 10:06:53 +0100

    nareszcie naprawione syfy

---
+++ wej01/src/test/java/wegetableManagerTest.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Greg on 15.03.2016.
+ */
+public class wegetableManagerTest extends TestCase {
+    @Test
+    public void testAdd() throws Exception {
+        
+    }
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+}
